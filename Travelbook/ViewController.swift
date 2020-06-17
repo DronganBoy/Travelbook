5d1dbd01b3a6bb0ce18069145a171196f56c7838
Jim A <65363926+DronganBoy@users.noreply.github.com>
2020-06-17 11:22:52 +0100

    Map set up complete

--- Travelbook/ViewController.swift
+++ Travelbook/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // sets to best accuracy
+        locationManager.requestWhenInUseAuthorization()// asks user to agree to use
+        locationManager.startUpdatingLocation()// starts use of location
+        
+        
     }
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude
+            , longitude: locations[0].coordinate.longitude)// sets up locations array with the coordinates
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1) // gives the accuracy required for location
+        let region = MKCoordinateRegion(center: location, span: span) // you canuse existing location in Debug like Apple HQ or input your own!
+        mapView.setRegion(region, animated: true) // calls map with the region and span selected
+    }
 
 }
 
